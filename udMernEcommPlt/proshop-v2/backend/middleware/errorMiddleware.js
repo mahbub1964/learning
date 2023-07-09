e0438d8ae9d013c244765fea7474d985157f241c
Syed Mahbubur Rahman <mahbubur.rahman.64@gmail.com>
2023-07-09 14:59:20 +0600

    Learn Udemy MERN eCommerce Platform - Backend (Order)

--- udMernEcommPlt/proshop-v2/backend/middleware/errorMiddleware.js
+++ udMernEcommPlt/proshop-v2/backend/middleware/errorMiddleware.js
@@ -1,6 +1,6 @@
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.errer(404);
+  res.status(404); //res.error(404);
   next(error);
 };
 
--- udMernEcommPlt/proshop-v2/backend/server.js
+++ udMernEcommPlt/proshop-v2/backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 //import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.get('/', (req, res) => { res.send('API is running...'); });
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 // app.get('/api/products', (req, res) => { res.json(products); });
 // app.get('/api/products/:id', (req, res) => {
